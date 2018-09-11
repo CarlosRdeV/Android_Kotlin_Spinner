251299d3a78193984dadb6b9c79d6d8f1611deb2
Carlos Rivera <carlosrdev@gmail.com>
2018-09-11 00:09:29 -0500

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.oldspice.spinnerapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var option: Spinner
+    lateinit var result: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        option = findViewById(R.id.sp_option) as Spinner
+        result = findViewById(R.id.tv_result) as TextView
+
+        var options = arrayOf("Option 1", "Option 2", "Option 3")
+
+        option.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, options)
+
+        option.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                result.text = "Please select an option"
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                result.text = options.get(position)
+
+            }
+        }
+    }
+}
